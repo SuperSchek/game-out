57f45560e5ab8d6132a7de46bfa9d0d1b3ee5874
tom_kaal <tommiekaal@gmail.com>
2015-05-28 20:12:58 +0200

    fixed angular(incl routes). Also made som empty html pages in advance

--- client/javascripts/app.js
+++ client/javascripts/app.js
@@ -1,16 +1,52 @@
-/*global angular, BookListCtrl, BookDetailCtrl */
+/*global angular, scoreCtrl, BookDetailCtrl */
+/*jslint node: true */
+/*jshint strict:false */
 
+"use strict";
 
 /**
- *
- * Writing AngularJS Documentation
- *
- * @see https://github.com/angular/angular.js/wiki/Writing-AngularJS-Documentation
  * @see http://docs.angularjs.org/guide/concepts
  */
-var nameApp = angular.module('nameApp', []);
-nameApp.controller('scoreCtrl', function ($scope){
-    console.log(users);
-    $scope.users = users;
-    console.log($scope.users);
-});
+//var myApp = angular.module('myApp', ['ngRoute'])
+myApp.config(['$routeProvider', function ($routeProvider) {
+
+        //login
+        $routeProvider.when('/login', {
+            templateUrl: 'partials/login.html',
+            controller: 'loginCtrl'
+        });
+        // homepage
+        $routeProvider.when('/home', {
+            templateUrl: 'partials/home.html',
+            controller: 'homeCtrl'
+        });
+        // the game
+        $routeProvider.when('/game', {
+            templateUrl: 'partials/game.html',
+            controller: 'gameCtrl'
+        });
+        //faq
+        $routeProvider.when('/faq', {
+            templateUrl: 'partials/faq.html',
+            controller: 'faqCtrl'
+        });
+        //leaderboard
+        $routeProvider.when('/leaderboard', {
+            templateUrl: 'partials/leaderboard.html',
+            controller: 'leaderboardCtrl'
+        });
+        //myprofile
+        $routeProvider.when('/myprofile', {
+            templateUrl: 'partials/myprofile.html',
+            controller: 'myprofileCtrl'
+        });
+        //searchfriends
+        $routeProvider.when('/searchfriends', {
+            templateUrl: 'partials/searchfriends.html',
+            controller: 'searchfriendsCtrl'
+        });
+        // When no valid route is provided
+        $routeProvider.otherwise({
+            redirectTo: "/login"
+        });
+    }]);
